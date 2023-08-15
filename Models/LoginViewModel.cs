1ea0e33403567ef78dd6508007efbefede8dd50d
Lars Appel <lasse.appel@gmail.com>
2023-08-15 23:43:46 +0200

    Refactor: Improve code readability

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -5,10 +5,12 @@ namespace MVCDemo.Models;
 public class LoginViewModel
 {
     [Required]
+    [Display(Name = "Username")]
     public string? Username { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
+    [Display(Name = "Password")]
     public string? Password { get; set; }
     public string? ReturnUrl { get; set; }
 }
